0e8ce3190276b822eda160dffdcf05bc47d099b9
pjo901018 <pjo901018@korea.ac.kr>
2017-05-05 09:18:48 +0900

    spring boot configuration

---
+++ src/main/java/com/asuscomm/yangyinetwork/websocket/config/WebSocketConfig.java
@@ -0,0 +1,7 @@
+package com.asuscomm.yangyinetwork.websocket.config;
+
+/**
+ * Created by jaeyoung on 2017. 5. 5..
+ */
+public class WebSocketConfig {
+}
---
+++ src/main/java/com/asuscomm/yangyinetwork/websocket/utils/CustomChannelInterceptor.java
@@ -0,0 +1,40 @@
+package com.asuscomm.yangyinetwork.websocket.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptorAdapter;
+
+/**
+ * Created by jaeyoung on 2017. 5. 5..
+ */
+
+@Slf4j
+public class ChannelInterceptor extends ChannelInterceptorAdapter {
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+
+        log.info("preSend message=[{}], channel=[{}]", message, channel);
+
+//		StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+//
+//		if(StompCommand.CONNECT.equals(accessor.getCommand())){
+//
+//		}
+
+        return message;
+    }
+
+    @Override
+    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+
+        log.info("postSend message=[{}], channel=[{}], sent=[{}]", message, channel, sent);
+
+//		StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+//
+//		if(StompCommand.CONNECT.equals(accessor.getCommand())){
+//
+//		}
+    }
+}
